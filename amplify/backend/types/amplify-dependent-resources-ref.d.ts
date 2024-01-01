fbe2b38012acbe10e438dd90963e4747d82dbff9
HenriVIII1509 <62750489+HenriVIII1509@users.noreply.github.com>
2024-01-01 19:45:31 +0000

    Setup of Amplify cli and auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "squadmanagement93cc08bc": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
